3045cfb270217aea818308b08436d05c52cfb392
Pragya <pragyadiptasaha@gmail.com>
2026-09-06 00:31:40 +0530

    maths

---
+++ 1-basics/6-nummaths.js
@@ -0,0 +1,37 @@
+const score=200
+//console.log(score);
+
+const balance=new Number(50)
+//console.log(balance);
+
+//console.log(balance.toString().length);
+//console.log(balance.toFixed(2));
+
+const num=127.4791
+//console.log(num.toPrecision(3));
+
+const n=1000000
+//console.log(n.toLocaleString('en-IN'));
+
+
+
+//maths//
+
+console.log(Math);
+//console.log(Math.abs(-4));
+//console.log(Math.round(5.7));
+//console.log(Math.ceil(5.3));
+//console.log(Math.floor(5.4));
+//console.log(Math.max(3,8,5));
+
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+console.log((Math.random()*10)+1);
+
+const min=15
+const max=25
+
+console.log(Math.floor(Math.random()*(max-min+1)));
+console.log(Math.floor(Math.random()*(max-min+1))+min);
+
+
