08218d69883c81367a04dd6fb2015de0795c45d1
Pragya <pragyadiptasaha@gmail.com>
2026-09-06 02:45:12 +0530

    date and time

---
+++ 1-basics/7-datetime.js
@@ -0,0 +1,29 @@
+let date=new Date()
+//console.log(date.toString());
+//console.log(date.toISOString());
+//console.log(date.toJSON());
+//console.log(date.toDateString());
+//console.log(date.toLocaleString());
+//console.log(typeof date);
+
+let d=new Date("6-09-2026")
+//console.log(d.toDateString());
+
+let timestamp=Date.now
+//console.log(timestamp());
+//console.log(d.getTime());
+//console.log(Math.floor(Date.now()/1000));
+
+let newdate=new Date
+//console.log(newdate);
+console.log(newdate.getMonth()+1);
+console.log(newdate.getDay());
+
+newdate.toLocaleString('default',{
+   weekday:"long"
+})
+
+
+
+
+
